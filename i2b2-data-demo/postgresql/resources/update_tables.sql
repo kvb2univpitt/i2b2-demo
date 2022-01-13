489257729abbca494cf30e13a2bc51c9d79e723a
Kevin Bui <kvb2univpitt@gmail.com>
2022-01-13 12:46:55 -0500

    Fixed hostname.

--- i2b2-data-demo/postgresql/resources/update_tables.sql
+++ i2b2-data-demo/postgresql/resources/update_tables.sql
@@ -1,6 +1,6 @@
 -- using docker container name as host
-UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo-postgresql:9090/i2b2/services/QueryToolService/' WHERE cell_id  = 'CRC';
-UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo-postgresql:9090/i2b2/services/FRService/' WHERE cell_id  = 'FRC';
-UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo-postgresql:9090/i2b2/services/OntologyService/' WHERE cell_id  = 'ONT';
-UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo-postgresql:9090/i2b2/services/WorkplaceService/' WHERE cell_id  = 'WORK';
-UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo-postgresql:9090/i2b2/services/IMService/' WHERE cell_id  = 'IM';
+UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo:9090/i2b2/services/QueryToolService/' WHERE cell_id  = 'CRC';
+UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo:9090/i2b2/services/FRService/' WHERE cell_id  = 'FRC';
+UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo:9090/i2b2/services/OntologyService/' WHERE cell_id  = 'ONT';
+UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo:9090/i2b2/services/WorkplaceService/' WHERE cell_id  = 'WORK';
+UPDATE pm_cell_data SET url = 'http://i2b2-core-server-demo:9090/i2b2/services/IMService/' WHERE cell_id  = 'IM';
